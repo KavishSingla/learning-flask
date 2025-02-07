14f128a1cf14c93225575656d6d28d0817138067
Your Name <your.email@example.com>
2025-02-07 14:39:03 +0000

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, Devansh!'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', debug=True)
